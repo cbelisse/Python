00c3b9b911e055a5d32bc5672134005165675651
Cleber Belisse <cleberbelisse@gmail.com>
2023-03-27 10:28:34 -0300

    Organizing Documents
    Moving two documents to the "List 1" folder.

---
+++ Several Codes/List 1/List_1_EX_7.py
@@ -0,0 +1,13 @@
+'''
+Titulo: Multiplicador de dois números
+Data: 04/03/2023
+Autor: Cleber Belisse
+Descrição: Multiplica dois números
+'''
+#7)	Monte um programa que exiba a multiplicação de dois números inteiros.
+numero1 = 5
+numero2 = 10
+
+resultado = numero1 * numero2
+
+print(resultado)
