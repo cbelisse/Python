2b7e808892d90c013213ad56ccc9babe13f1594e
Cleber Belisse <cleberbelisse@gmail.com>
2023-03-27 10:23:50 -0300

    Create List_1_EX_8.py

---
+++ Several Codes/List 3/List_1_EX_8.py
@@ -0,0 +1,13 @@
+'''
+Titulo: Divisor de dois números
+Data: 04/03/2023
+Autor: Cleber Belisse
+Descrição: Divide dois números
+'''
+#8)	Monte um programa que exiba a divisão de dois números inteiros.
+numero1 = 8
+numero2 = 4
+
+resultado = numero1 / numero2
+
+print(resultado)
