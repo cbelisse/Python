dbc585f11e1dcd9ede2850ee0e59c117d4128e3d
Cleber Belisse <cleberbelisse@gmail.com>
2023-03-28 09:00:30 -0300

    Moving and renaming Lista_1_EX_1_A_5
    Renaming document Lista_1_EX_1_A_5 to List_1_EX_1_A_5 and moving it to the List 1 folder.

---
+++ Several Codes/List 1/List_1_EX_1_A_5.py
@@ -0,0 +1,61 @@
+'''
+Exercícios da lista 1
+1 a 5
+1 - Identificar operadores
+2 - Nome de variáveis
+3 - Tipo de variáveis
+4 - Resultado de código
+5 - Análise de código
+Data 04/03/2023
+Autor: Cleber Belisse
+
+1)	Quais destes são operadores e quais são valores:
+
+  *   é operador
+'ola' é string
+-15.3 é int
+  -   é operador
+  /   é operador
+  +   é operador
+  22  é int
+
+
+2)	Quais destes nomes podem ser utilizados para criação de variáveis:
+      Teste - Possível
+     'teste' - Não é possível
+     _teste  - Possível
+    42_teste - Impossível
+ joaoCardoso - Possível
+    1Caixa   - Impossível
+'nomeCompleto'- Impossível
+botao4LigaLampada - Possível
+
+
+3)	Indique qual o tipo de cada variável abaixo:
+
+SENAI - Não pode
+ 12   - int
+ "a"  - string
+ 1.5  - float
+ True - boolean
+ '55' - string
+ 'False' - string
+"Verdadeiro ou Falso?" - string
+
+
+4)	Qual o resultado do código a seguir:
+alunos = 15
+alunos = alunos + 1
+print(alunos)
+
+Resultado: 16
+
+
+5)	Qual o resultado do código a seguir:
+alunos = ‘15’
+alunos = alunos + ‘1’
+print(alunos)
+
+Resposta: 151
+
+'''
