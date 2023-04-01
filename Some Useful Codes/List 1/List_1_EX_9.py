c2f0305ad2eb5c47cdadd4e2a636b67a51aed9a8
Cleber Belisse <124924550+cbelisse@users.noreply.github.com>
2023-04-01 09:59:37 -0300

    Subtraction operator
    This document has a code with subtraction operator

---
+++ Some Useful Codes/List 1/List_1_EX_9.py
@@ -0,0 +1,13 @@
+'''
+Titulo: Subtrator de dois números
+Data: 04/03/2023
+Autor: Cleber Belisse
+Descrição: Subtrai dois números
+'''
+#9)	Monte um programa que exiba a subtração de dois números inteiros.
+numero1 = 10
+numero2 = -3
+
+resultado = numero1 - numero2
+
+print(resultado)
