c900be4ff66dd77d9f9c314d018fba04c373fefb
Cleber Belisse <cleberbelisse@gmail.com>
2023-04-04 09:24:55 -0300

    Multiplying numbers reported by the user
     In this document you find the code for muliplying numbers reported by the user.

---
+++ Some Useful Codes/List 1/List_1_EX_11.py
@@ -0,0 +1,20 @@
+'''
+Titulo: Multiplicando números informados pelo usuário (Multiplying numbers reported by the user)
+Data: 05/03/2023
+Autor: Cleber Belisse
+Descrição: In this document you find the code for muliplying numbers reported by the user.
+'''
+#11) Monte um programa que exiba a multiplicação de dois números inteiros informados pelo usuário.
+print('Seja bem vindo ao programa Multiplicador \
+de numeros informados pelo usuario')
+
+print('Por favor, informe o primeiro numero')
+numero1 = int(input())
+
+print('Informe o segundo numero')
+numero2 = int(input())
+
+resultado = numero1 * numero2
+
+print('O resultado da sua multiplicação será '+
+str(resultado))
