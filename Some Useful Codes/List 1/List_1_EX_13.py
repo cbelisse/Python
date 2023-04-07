f903391285483bfd10434405f5ae87b699793212
Cleber Belisse <cleberbelisse@gmail.com>
2023-04-07 10:02:49 -0300

    Subtraindo numeros informados pelo usuário
    This document has the code to make subtraction of numbers entered ny the user.

---
+++ Some Useful Codes/List 1/List_1_EX_13.py
@@ -0,0 +1,19 @@
+'''
+Titulo: Subtrator de numeros informados pelo usuario
+Data: 05/03/2023
+Autor: Cleber Belisse
+Descrição: Subtrai dois números inteiros
+informados pelo usuario
+'''
+#13)	Monte um programa que exiba a subtração de dois números inteiros informados pelo usuário.
+print('Seja bem vindo ao programa Subtrator de numeros informados pelo usuario')
+
+print('Por favor, informe o primeiro numero')
+numero1 = int(input())
+
+print('Informe o segundo numero')
+numero2 = int(input())
+
+resultado = numero1 - numero2
+
+print('O resultado da sua subtração será ' + str(resultado))
