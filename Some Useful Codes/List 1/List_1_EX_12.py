d8d47e53298ea4f203421729fd2c5748059e73ce
Cleber Belisse <cleberbelisse@gmail.com>
2023-04-07 09:47:49 -0300

    Division of number entered by the user
    This document has the code to make the division of numbers entered by the user.

---
+++ Some Useful Codes/List 1/List_1_EX_12.py
@@ -0,0 +1,21 @@
+'''
+Titulo: Divisão de dois números informados pelo usuario (Division of two numbers entered by the yser)
+Data: 05/03/2023
+Autor: Cleber Belisse
+Descrição: Este documento tem o código para dividir dois números inseridos pelo usuário
+(This document has the code to make the division of two numbers entered by the user).
+informados pelo usuario
+'''
+#12) Monte um programa que exiba a divisão de dois números inteiros informados pelo usuário.
+print('Seja bem vindo ao programa Divisor \
+de numeros informados pelo usuario')
+
+print('Por favor, informe o primeiro numero')
+numero1 = int(input())
+
+print('Informe o segundo numero')
+numero2 = int(input())
+
+resultado = numero1 / numero2
+
+print('O resultado da sua divisão será ' + str(resultado))
