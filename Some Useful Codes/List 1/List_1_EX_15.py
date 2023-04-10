48e99e8119bcdeab5c580e0bf5315759d510d40c
Cleber Belisse <cleberbelisse@gmail.com>
2023-04-10 20:29:14 -0300

    List_1_EX_15 - Calculadora de média
    Calcula a média das notas escolares de um aluno.

---
+++ Some Useful Codes/List 1/List_1_EX_15.py
@@ -0,0 +1,22 @@
+'''
+Data: 05/03/2023
+Autor: Cleber Belisse
+Descrição: Calcula a média de duas notas
+'''
+'''
+15)	Uma professora gostaria um programa para 
+auxiliá-la a montar a média final de seus alunos. Sabendo que são 2 notas por semestre, monte um programa que
+através das notas informadas pelo usuário mostre a sua média final. Não esqueça de manter uma boa interface 
+com o usuário!!
+'''
+print('Seja bem vindo ao programa ' + titulo)
+
+print('Por favor, informe a primeira nota')
+nota1 = float(input())
+
+print('Informe a segunda nota')
+nota2 = float(input())
+
+media = (nota1 + nota2) / 2
+
+print('Sua média é ' + str(media))
