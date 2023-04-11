82d06f23a82d2d948b3fd4af6b2e5138bcdc239c
Cleber Belisse <cleberbelisse@gmail.com>
2023-04-11 11:02:52 -0300

    Subtraction of two numbers by user input
     In this code the user enters two numbers and the program subtract them.

--- Some Useful Codes/List 1/List_1_EX_13.py
+++ Some Useful Codes/List 1/List_1_EX_13.py
@@ -1,9 +1,9 @@
 '''
-Titulo: Subtrator de numeros informados pelo usuario
+Titulo: Subtraindo dois números informados pelo usuario (Subtraction of two numbers by user imput)
 Data: 05/03/2023
 Autor: Cleber Belisse
-Descrição: Subtrai dois números inteiros
-informados pelo usuario
+Descrição: Neste código o usuário insere dois números e o programa faz a subtração entre eles. (In this code the user entered two numbers and program
+subtract them).
 '''
 #13)	Monte um programa que exiba a subtração de dois números inteiros informados pelo usuário.
 print('Seja bem vindo ao programa Subtrator de numeros informados pelo usuario')
