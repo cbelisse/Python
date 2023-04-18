d47148b39c62a30f281a5586b836d6045d41b79d
Cleber Belisse <cleberbelisse@gmail.com>
2023-04-18 09:30:03 -0300

    Free public transport tickets for the elderly
    The user enters his age and the program verifies if he is entitled to free public transport tickets.

--- Some Useful Codes/List 2/List_2_EX_2.py
+++ Some Useful Codes/List 2/List_2_EX_2.py
@@ -1,7 +1,10 @@
 '''
-Data: 17/0/04/2023
+Data: 17/04/2023
 Autor: Cleber Belisse
-Descrição: Liberação de gratuidade no transporte público
+Descrição: Gratuidade na passagem do transporte público para idosos (Free public transport tickets for the elderly)
+O usuário insere a sua idade e o programa verifica se ele tem direito a gratuidade na passagem do transporte público
+(The user enters his age and the program verified if he is entitled to free public transport ticket).
+
 Uma empresa de transporte público quer fazer um sistema automático para identificar se o usuário terá gratuidade no
 transporte ou não.
 Faça um programa que pergunte a idade do usuário, se ele tiver 65 anos ou mais irá informar que ele tem gratuidade
