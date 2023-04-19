5ee70966f51a855ee137f597a5d3500cc01285a7
Cleber Belisse <cleberbelisse@gmail.com>
2023-04-19 09:13:55 -0300

    Assessing student performance
     Teachers enter students grades and program calculates the average of those students grades. After calculating the grade point average, the program informs if the student is approved, failed or goes to summer school.

--- Some Useful Codes/List 2/List_2_EX_3.py
+++ Some Useful Codes/List 2/List_2_EX_3.py
@@ -1,7 +1,12 @@
 '''
 Data: 06/03/2023
 Autor: Cleber Belisse
-Descrição: Avaliando desempenho de alunos
+Descrição: Avaliando desempenho de alunos (Assessing student performance)
+Os professores inserem as notas dos alunos e o programa calcula a média das nostas destes alunos. Após calcular a média
+o programaa informa se o aluno está aprovado, reprovado ou vai para a recuperação.
+Teachers enter students grades and program calculates the average of those students grades. After calculating the grade
+point average, the program informs if the student is approved, failed or goes to summer school.
+
     Faça um programa que pede duas notas de um aluno. Em seguida ele deve calcular a média do aluno e dar o seguinte resultado:
 •	Aprovado  Acima de 7
 •	Reprovado  Abaixo de 5
