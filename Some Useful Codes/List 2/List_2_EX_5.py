c34a01c47f7a47fac3b5d203130f5fc2667dfc57
Cleber Belisse <cleberbelisse@gmail.com>
2023-04-26 09:36:02 -0300

    Calculating salary increase
    This program calculates the percentage of increase in salary according to salary range.
    
    • Salary up to $1,499.00 - increase of 25%
    • Salary Between R$1,500.00 and R$1,999.99 -Increase of 20%
    • Salary Between R$2,000.00 and R$2,999.99 -  Increase of 15%
    • Salary Between R$3,000.00 and R$4,999.99 - increase of 10%
    • Salary greater than BRL 5,000.00 -  increase of 5%

--- Some Useful Codes/List 2/List_2_EX_5.py
+++ Some Useful Codes/List 2/List_2_EX_5.py
@@ -1,15 +1,16 @@
 '''
-Data: 07/03/2023
+Data: 25/03/2023
 Autor: Cleber Belisse
-Descrição: Calculando o reajuste salarial conforme salário.
+Descrição: Caulculando aumento salarial (Calculating salary increase)
+Este programa calcula o aumento no salário conforme faixa salarial ( This program calculates the percentage of increase in salary according to salary range).
+• Salary up to $1,499.00  increase of 25%
+• Salary Between R$1,500.00 and R$1,999.99 Increase of 20%
+• Salary Between R$2,000.00 and R$2,999.99  Increase of 15%
+• Salary Between R$3,000.00 and R$4,999.99  increase of 10% 
+• Salary greater than BRL 5,000.00  5% increase
 
 Uma empresa pretende fazer um reajuste salarial para os funcionários e precisa da sua ajuda para criar um programa.
-Ficou definido os seguintes reajustes:
-•	Salário Abaixo de R$1.500,00  Aumento de 25%
-•	Salário Entre de R$1.500,00 e R$1.999,99  Aumento de 20%
-•	Salário Entre de R$2.000,00 e R$2.999,99  Aumento de 15%
-•	Salário Entre de R$3.000,00 e R$4.999,99  Aumento de 10%
-•	Salário Acima de R$5.000,00  Aumento de 5%
+Ficou definido os ajustes confome lista acima.
 
 Faça um programa que pergunte ao usuário qual seu Salário Atual e mostre ao usuário:
 1.	O salário atual;
@@ -21,36 +22,22 @@ titulo = 'Cálculo de reajuste salarial'
 print('Seja bem vindo ao programada chamado '+ titulo)
 print('Para calcular o reajuste, por favor, informe o salário atual.')
 salario_atual = float(input('R$:'))
-if salario_atual < 1500.00:
-    porcentagem_aumento = 1.25
-    salario_final = salario_atual * porcentagem_aumento
-    aumento_em_reais = salario_final - salario_atual
-    print('Você recebeu 25% de aumento e o seu salário que hoje é de ' + str(salario_atual) + 'reais, passará para ' + str(salario_final) +
-          ' reais, um aumento de ' + str(aumento_em_reais) + ' reais.')
-elif salario_atual >= 1500.00 and salario_atual <2000.00:
-    porcentagem_aumento = 1.20
-    salario_final = salario_atual * porcentagem_aumento
-    aumento_em_reais = salario_final - salario_atual
-    print('Você recebeu 20% de aumento e o seu salário que hoje é de ' + str(salario_atual) + 'reais, passará para ' + str(salario_final) +
-          ' reais, um aumento de ' + str(aumento_em_reais) + ' reais.')
-elif salario_atual >= 2000.00 and salario_atual <3000.00:
-    porcentagem_aumento = 1.15
-    salario_final = salario_atual * porcentagem_aumento
-    aumento_em_reais = salario_final - salario_atual
-    print('Você recebeu 15% de aumento e o seu salário que hoje é de ' + str(salario_atual) + ' reais, passará para ' + str(salario_final) +
-          ' reais, um aumento de ' + str(aumento_em_reais) + ' reais.')
-elif salario_atual >= 3000.00 and salario_atual <5000.00:
-    porcentagem_aumento = 1.10
-    salario_final = salario_atual * porcentagem_aumento
-    aumento_em_reais = salario_final - salario_atual
-    print('Você recebeu 10% de aumento e o seu salário que hoje é de ' + str(salario_atual) + ' reais, passará para ' + str(salario_final) +
-          ' reais, um aumento de ' + str(aumento_em_reais) + ' reais.')
+
+if salario_atual < 1500:
+    aumento = 25
+elif salario_atual < 2000:
+    aumento = 20
+elif salario_atual < 3000:
+    aumento = 15
+elif salario_atual < 5000:
+    aumento = 10
 else:
-    porcentagem_aumento = 1.05
-    salario_final = salario_atual * porcentagem_aumento
-    aumento_em_reais = salario_final - salario_atual
-    print('Você recebeu 5% de aumento e o seu salário que hoje é de ' + str(salario_atual) + ' reais, passará para ' + str(salario_final) +
-          ' reais, um aumento de ' + str(aumento_em_reais) + ' reais.')
+    aumento = 5
+
+aumento_em_reais = salario_atual * aumento / 100
+salario_final = salario_atual + aumento_em_reais
+print('Seu salário atual é de R$' + str(salario_atual) + ', você receberá ' + str(aumento) +
+      '% de aumento ou R$' + str(aumento_em_reais) +', então seu novo salário será R$ ' +str(salario_final) + '.')
 
 
 
