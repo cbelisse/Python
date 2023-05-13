faeb55aadfc49dbffd6644012be5115bece693db
Cleber Belisse <124924550+cbelisse@users.noreply.github.com>
2023-05-13 14:49:44 -0300

    Cadast. user como dicion em list após validar CPF

---
+++ Some Useful Codes/List 5/Lista_5_Ex_7.py
@@ -0,0 +1,9 @@
+'''
+Data: 13/05/2023
+Autor: Cleber
+7)	Crie uma função que receba o nome, idade e CPF e então cadastre como um dicionário (dict) em uma lista de usuários.
+O programa deverá permitir adicionar usuário, excluir usuário e listar usuários.
+Valide se o CPF é válido ou não antes de aceitar o cadastro.
+
+'''
+titulo = 'Cadastra usuário '
